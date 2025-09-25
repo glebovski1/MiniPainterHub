dabfce2b855da8ee66ec57476f7e9a50e364a2e8
gleb Usov <51403995+glebovski1@users.noreply.github.com>
2025-09-24 22:12:46 -0400

    Fix auto cycling for image carousel

--- MiniPainterHub.WebApp/wwwroot/JSHelpers/domHelpers.js
+++ MiniPainterHub.WebApp/wwwroot/JSHelpers/domHelpers.js
@@ -1,3 +1,14 @@
-﻿window.domHelpers = {
-    getHeight: (el) => el ? el.offsetHeight : 0
+window.domHelpers = window.domHelpers || {};
+
+window.domHelpers.getHeight = (el) => (el ? el.offsetHeight : 0);
+
+window.domHelpers.initCarousel = (element) => {
+    if (!element || !window.bootstrap || !window.bootstrap.Carousel) {
+        return;
+    }
+
+    const instance = window.bootstrap.Carousel.getInstance(element)
+        || new window.bootstrap.Carousel(element);
+
+    instance.cycle();
 };
