49b2c08b67c5fc9ad8a417e2dcadad16bdd03f06
Glebovski1 <uslepwel@gmail.com>
2026-02-27 13:53:11 -0500

    Remove unused WebApp CreateImagePostDto duplicate

--- MiniPainterHub.WebApp/DTOs/CreateImagePostDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiniPainterHub.WebApp.DTOs
-{
-    public class CreateImagePostDto
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
-
-    }
-}
